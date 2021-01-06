4e9b3517ce633ede5ca1d4eac1b52d60d1c42e3b
1992w <jiangbing_yang@163.com>
2021-01-06 19:57:08 +0800

    重构代码

--- src/ProxyPool.Core/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/ProxyPool.Core/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.Extensions.Configuration;
+using ProxyPool.Core.Net;
 using ProxyPool.Core.Pipeline;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddProxyCheck(this IServiceCollection services)
+        {
+            services.AddScoped<IProxyCheck, DefaultProxyCheck>();
+            return services;
+        }
+
         public static IServiceCollection AddProxyChannel(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<IProxyPipeline, RedisProxyPipieline>();
